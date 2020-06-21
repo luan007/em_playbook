c5500e09b37bc59d7e7bcab58867bd1d0e62bab3
luan007 <luancfan@hotmail.com>
2020-06-21 11:15:44 +0800

    working signal subsys

--- tiny-os-2/defs.h
+++ tiny-os-2/defs.h
@@ -4,6 +4,14 @@
 #include <list>
 #include <Preferences.h>
 
+#define DBG_LEVEL_LOG 0
+
+void DEBUG(const char *module, const char *stuff, int LEVEL = DBG_LEVEL_LOG)
+{
+    auto msg = String("[DBG] ") + module + " : " + stuff;
+    Serial.println(msg.c_str());
+}
+
 Preferences _signal_store;
 
 typedef struct schedule
@@ -36,9 +44,11 @@ void schedule_recompute_all()
 {
     for (auto i : schedules)
     {
-        i->compute != NULL && i->compute();
+        if (i->compute != NULL)
+        {
+            i->compute();
+        }
     }
-    return NULL;
 }
 
 //this is the worst, things are broken, errmsg must be printed
@@ -62,10 +72,13 @@ typedef struct signal
     const char *name;
     const char *fallback_msg;
     int visibility;
-    int resolved;
     int presist_behavior;
+    int resolved;
     int value;
+    int _saved_value; //to ensure not writing too many times during loop
 };
+
+#define SIGNAL(NAME, default_msg, visibility, presist_behavior, default_value) struct signal NAME = {#NAME, default_msg, visibility, presist_behavior, 0, default_value};
 
 std::list<struct signal *> signals;
 
@@ -90,6 +103,12 @@ struct signal *signal_get(const char *name)
 struct signal *signal_raise(const char *name, int v, const char *fallback_msg)
 {
     auto sig = signal_get(name);
+    return sig;
+}
+
+//call this when possible!
+struct signal *signal_raise(struct signal *sig, int v, const char *fallback_msg)
+{
     if (sig == NULL)
     {
         return NULL;
@@ -103,7 +122,6 @@ struct signal *signal_raise(const char *name, int v, const char *fallback_msg)
             sig->fallback_msg = fallback_msg;
         }
     }
-    return sig;
 }
 
 void signal_resolve(struct signal *sig)
@@ -120,7 +138,9 @@ void signal_presist_init()
         {
             _signal_store.begin("signal_store", false);
             i->value = _signal_store.getInt(i->name, 0);
-            i->resolved = _signal_store.getInt(strcat("resolved_", i->name), 0);
+            i->resolved = _signal_store.getInt((String("resolved_") + i->name).c_str(), 0);
+            i->_saved_value = i->value;
+            DEBUG("SIGNAL", (String("Load ") + i->name + " = " + i->value).c_str());
         }
     }
     _signal_store.end();
@@ -137,11 +157,13 @@ void signal_presist_update()
         {
             signal_resolve(i);
         }
-        else if (i->presist_behavior == SIGNAL_PRESIST_POWERLOSS)
+        else if (i->presist_behavior == SIGNAL_PRESIST_POWERLOSS && (i->value != i->_saved_value))
         {
             _signal_store.begin("signal_store", false); //preferences.h already checks _started for us
             _signal_store.putInt(i->name, i->value);
-            _signal_store.putInt(strcat("resolved_", i->name), i->resolved);
+            _signal_store.putInt((String("resolved_") + i->name).c_str(), i->resolved);
+            i->_saved_value = i->value;
+            DEBUG("SIGNAL", (String("Save ") + i->name + " = " + i->value).c_str());
         }
     }
     _signal_store.end();
--- tiny-os-2/tiny-os-2.ino
+++ tiny-os-2/tiny-os-2.ino
@@ -8,18 +8,32 @@
 
 void TaskMain(void *pvParameters);
 
+SIGNAL(TEST, "Test signal", SIGNAL_VIZ_APP | SIGNAL_VIZ_OS | SIGNAL_VIZ_USER, SIGNAL_PRESIST_POWERLOSS, 0)
+
 void setup()
 {
   Serial.begin(115200);
 
+  signal_register(&TEST);
+
+  signal_presist_init();
+
+  signal_raise(&TEST, 2, NULL);
+
+
+
+
+  //start os
   xTaskCreatePinnedToCore(
       TaskMain, "TaskMain",
       1024, NULL, 2, NULL, CPU);
+
 }
 
 void loop()
 {
   // Serial.println("Test from Loop");
+  signal_presist_update();
 }
 
 void TaskMain(void *pvParameters)
