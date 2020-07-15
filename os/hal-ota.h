840993994b280aa98610bdc31bd5a26afb71e5b7
JAukey <jackaukey@126.com>
2020-07-15 18:04:07 +0800

    Changes to be committed: 	new file:   os/hal-ota.h

---
+++ os/hal-ota.h
@@ -0,0 +1,94 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Update.h>
+#include <WiFiAP.h>
+
+const char* host = "esp32";
+const char* ssid = "EM-Paper";
+//const char* password = "emergeLAB";
+WebServer server(80);
+const char* serverIndex = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
+String OTADebug;
+boolean ledTrigger = false;
+long ota_time = millis();
+bool ota_config() {
+  DEBUG("START OTA", "");
+  WiFi.softAP(ssid);
+  IPAddress myIP = WiFi.softAPIP();
+  if (!MDNS.begin(host)) { //http://esp32.local
+    DEBUG("Error setting up MDNS responder!", "");
+  }
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      OTADebug  = "  Start Upload The File \n\n";
+      OTADebug += "  DO NOT TURN OFF THE DEVICE !!!\n\n";
+      OTADebug += "  " + String(upload.filename.c_str()) + " ";
+      display_dbg_print(OTADebug);
+      if (!Update.begin()) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      if (Update.remaining() % (128 * 1024) == 0) {
+        OTADebug += ".";
+        display_dbg_print(OTADebug);
+        ota_time = millis();
+      }
+      if (ledTrigger) {
+        LED_A_ON;
+      } else {
+        LED_A_OFF;
+      }
+      ledTrigger = !ledTrigger;
+      
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        OTADebug += "\n\n  Update Successed !!!";
+        OTADebug += "\n\n  System Restart !!!";
+        display_dbg_print(OTADebug);
+      } else {
+        OTADebug += "\n\n  Update Failed !!!";
+        OTADebug += "\n\n  System Restart !!!";
+        display_dbg_print(OTADebug);
+        Update.printError(Serial);
+        ESP.restart();
+      }
+      Serial.setDebugOutput(false);
+    } else {
+      //Serial.printf("Update Failed Unexpectedly (likely broken connection): status=%d\n", upload.status);
+      OTADebug += "\n\n  Update Failed !!!";
+      OTADebug += "\n\n   System Restart !!!";
+      display_dbg_print(OTADebug);
+      ESP.restart();
+    }
+  });
+  server.begin();
+  hal_io_loop();
+  sig_clear(&SIG_SW_CLICK, 0);
+  while (1) {
+    server.handleClient();
+    delay(1);
+    if (millis() - ota_time > (240 * 1000) || (SIG_SW_CLICK.triggered > 0 && SIG_SW_CLICK.value > 0))
+    {
+      OTADebug += "\n\n  Time Out Or Press The Button To Exit !!!";
+      display_dbg_print(OTADebug);
+      break;
+    }
+    hal_io_loop();
+  }
+  ESP.restart();
+}
