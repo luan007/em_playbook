1a2a7fe2f4c7dfc58e06bd914240ed8fa679141c
luan007 <luancfan@hotmail.com>
2020-07-16 17:54:33 +0800

    c

--- server/static/about/main.lua
+++ server/static/about/main.lua
@@ -2,4 +2,6 @@ local touch = load_int("about", "page")
 if touch > 0 then
     touch = touch - 1
 end
+sprint("TOUCH UPDATE")
+sprint(touch)
 smart_draw_r("/about/root.bin", 600, 2400, 0, 800 * touch, 600, 800, 0, 0, 0)
--- server/static/about/signal.lua
+++ server/static/about/signal.lua
@@ -4,5 +4,7 @@ if load_string("main", "APP") == "about" and sig_alert("TOUCH_CLICK") > 0 then
     if touch > 0 then
         touch = touch - 1
     end
+    sprint("TOUCH UPDATE")
+    sprint(touch)
     smart_draw_r("/about/root.bin", 600, 2400, 0, 800 * touch, 600, 800, 0, 0, 0)
 end
