451a547ccbcea0fba018a62f83373f7f08cf9f15
lulu-s <1192707005@qq.com>
2020-08-14 16:37:59 -0700

    add

--- proxy/main.js
+++ proxy/main.js
@@ -3,6 +3,8 @@ const fetch = require('node-fetch');
 var qs = require('querystring');
 const cheerio = require('cheerio')
 const htmlToText = require('html-to-text');
+const fs = require("fs");
+const YAML = require('json-to-pretty-yaml');
 
 async function fetchUrl(url) {
 	var a = await fetch(url);
@@ -24,32 +26,64 @@ async function fetchUrl(url) {
 	).map(v => v.trim());
 	// console.log(text.join())
 	text = text.join().replace(/,/g, ' ');
-	// text = text.split("。").replace(/^([`~!@#$^&*()=|{}':;',\[\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？])/i, '').join()
-	// text = text.split("，").replace(/^([`~!@#$^&*()=|{}':;',\[\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？])/i, '').join()
+	var reg = new RegExp(/^([`~!@#$^&*()=|{}':;',\[\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？])/, 'i');
+	text = text.split("。").map(v=>v.trim().replace(reg, '')).join("。")
+	text = text.split("，").map(v=>v.trim().replace(reg, '')).join("，")
 	return text.substring(0, 300);
 }
 
 var lastUpdate = 0;
 var interval = 1000 * 60 * 60 * 6;
+var datas = [];
 function get_page_0(params) {
 	if (Date.now() - lastUpdate < interval) return console.log("skip");
 	params.offset = 0;
 	params.action = "getmsg";
 	//    console.log(params);
 	var str = qs.stringify(params);
-	//    console.log(str);
+	   console.log(str);
 	fetch('https://mp.weixin.qq.com/mp/profile_ext?' + str).then(res => res.json()).then(async (json) => {
 		lastUpdate = Date.now();
 		var data = JSON.parse(json.general_msg_list).list;
 		//data.length = 3;
 		var contents = [];
-		//console.log(data);	
 		for (var i = 0; i < data.length; i++) {
 			contents[i] = await fetchUrl(data[i].app_msg_ext_info.content_url);
 			console.log(data[i]);
 			console.log(contents[i]);
 			console.log("---");
+
+			// datas.push({
+			// 	...data[i].app_msg_ext_info,
+			// 	details: contents[i],
+			// 	...data[i].comm_msg_info
+			// })
+
+			datas.push({
+				title: data[i].app_msg_ext_info.title,
+				author: data[i].app_msg_ext_info.author,
+				multi_app_msg_item_list: data[i].app_msg_ext_info.author.multi_app_msg_item_list,
+				details: contents[i],
+				datetime: data[i].comm_msg_info.datetime,
+				time: getMyDate(data[i].comm_msg_info.datetime).base,
+				content_url: data[i].app_msg_ext_info.content_url,
+			})
 		}
+
+		var str = JSON.stringify(datas);
+		fs.writeFile("json/google_data.json", str, function (err, data) {
+			if (err) {
+				console.error(err);
+			}
+			console.log("----------新增成功-------------");
+		});
+		var str = YAML.stringify(datas);
+		fs.writeFile("yaml/google_data.yaml", str, function (err, data) {
+			if (err) {
+				console.error(err);
+			}
+			console.log("----------新增成功-------------");
+		});
 		//console.log(contents);
 	});
 }
@@ -63,3 +97,26 @@ e.get("*", (req, res) => {
 });
 
 e.listen(9999);
+
+
+// 转换日期
+function getMyDate(str) {
+	var oDate = new Date(str),
+		oYear = oDate.getFullYear(),
+		oMonth = oDate.getMonth() + 1,
+		oDay = oDate.getDate(),
+		oHour = oDate.getHours(),
+		oMin = oDate.getMinutes(),
+		oSen = oDate.getSeconds(),
+		oBase =  oYear + '/' + oMonth + '/' + oDay
+	return {
+		years: oYear,
+		months: oMonth,
+		days: oDay,
+		hours: oHour,
+		minutes: oMin,
+		seconds: oSen,
+		base: oBase,
+		ms: oDate.getTime()
+	}
+}
